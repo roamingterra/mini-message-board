7e1562a0a66bf9102666cc7e61607e68acd84053
roamingterra <danielballerini1@gmail.com>
2023-08-03 13:10:20 -0400

    Add favicon and remove required message
    I installed the serve-favicon middleware to include a favicon in this
    Express app. With regards to the required message, this is a pseudo
    element I created in the new message form that appears when the user
    clicks submit and did not fill the required fields. I realized before
    deploying my app that, because the messages are absolutely positioned,
    they do not appear in the appropriate positions for all screen sizes. I
    decided to comment out this feature, and will potentially update this in
    the future. The focus right now is to practice deploying an Express app
    with a PostgreSQL database, so I did not want to waste any more time
    working on this.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const messageRoutes = require("./src/messages/routes");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+var favicon = require("serve-favicon");
 
 const path = require("path");
+
+app.use(favicon(path.join(__dirname, "src", "images", "favicon.ico"))); // middleware to include a favicon
 
 // View setup
 app.set("view engine", "ejs");
