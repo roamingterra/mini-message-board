f0871dfbfa2a2bdf55a36d2ffafd0dc79e5121d5
roamingterra <danielballerini1@gmail.com>
2023-08-03 15:19:37 -0400

    Configure database
    I had to configure my db.js file to use the DATABASE_URL environment
    variable to connect to the PostgreSQL database provided by Fly.io. I
    also updated my fly.toml file to contain the DATABASE_URL environment
    variable and points to the provided PostgreSQL database URL.

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
 // This file is for the database connection
 
 const Pool = require("pg").Pool;
+const { Client } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mini_message_board_messages",
-  password: "password",
-  port: 5432,
+// Database will use the DATABASE_URL environment variable to connect to the PostgreSQL database provided by Fly.io
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
 });
 
-module.exports = pool;
+module.exports = client;
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "mini_message_board_messages",
+//   password: "password",
+//   port: 5432,
+// });
+
+// module.exports = pool;
